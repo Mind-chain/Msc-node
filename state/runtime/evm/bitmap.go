c2512ddb24dc66fae2941a4ffeb940877a65df0b
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-02-23 16:18:27 +0600

    Update bitmap.go

--- state/runtime/evm/bitmap.go
+++ state/runtime/evm/bitmap.go
@@ -2,49 +2,47 @@ package evm
 
 import "github.com/0xPolygon/polygon-edge/helper/common"
 
-const (
-	bitmapSize = 8
-	pushOpcode = 0x60
-)
+const bitmapSize = 8
 
 type bitmap struct {
 	buf []byte
 }
 
 func (b *bitmap) isSet(i uint64) bool {
-	return len(b.buf) > 0 && int(i/bitmapSize) < len(b.buf) && b.buf[i/bitmapSize]&(1<<(i%bitmapSize)) != 0
+	return b.buf[i/bitmapSize]&(1<<(i%bitmapSize)) != 0
 }
 
 func (b *bitmap) set(i uint64) {
-	idx := int(i / bitmapSize)
-	if idx >= len(b.buf) {
-		b.buf = append(b.buf, make([]byte, idx-len(b.buf)+1)...)
-	}
-	b.buf[idx] |= 1 << (i % bitmapSize)
+	b.buf[i/bitmapSize] |= 1 << (i % bitmapSize)
 }
 
 func (b *bitmap) reset() {
 	for i := range b.buf {
 		b.buf[i] = 0
 	}
+
 	b.buf = b.buf[:0]
 }
 
 func (b *bitmap) setCode(code []byte) {
-	b.reset()
-	for i := 0; i < len(code); {
+	codeSize := len(code)
+	b.buf = common.ExtendByteSlice(b.buf, codeSize/bitmapSize+1)
+
+	for i := 0; i < codeSize; {
 		c := code[i]
+
 		if isPushOp(c) {
-			size := int(c-pushOpcode) + 1
-			if i+size >= len(code) {
-				break // Avoid potential out-of-bounds access
+			// push op
+			i += int(c) - 0x60 + 2
+			if c == PUSH1 && code[i-1] == 0x00 {
+				b.set(uint64(i))
 			}
-			for j := 1; j <= size; j++ {
-				b.set(uint64(i + j))
-			}
-			i += size + 1
+		} else if c == ADD {
+			b.set(uint64(i))
+			i++
 		} else {
 			if c == JUMPDEST {
+				// jumpdest
 				b.set(uint64(i))
 			}
 			i++
@@ -52,6 +50,7 @@ func (b *bitmap) setCode(code []byte) {
 	}
 }
 
-func isPushOp(opcode byte) bool {
-	return opcode >= pushOpcode && opcode < pushOpcode+32
+func isPushOp(i byte) bool {
+	// From PUSH1 (0x60) to PUSH32(0x7F)
+	return i>>5 == 3
 }
